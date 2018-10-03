b62c241119c3976f68becb152e90408a80834fed
André Yamamoto <aayandre94@gmail.com>
2018-10-03 13:54:23 -0300

    Fiz algumas alterações para o maven criar um war, agora para rodar localmente o projeto precisa ser feito algumas alterações nas configurações do eclipse (estou usando eclipse depois eu vejo se roda no netbeans

--- petchopp/src/main/java/com/senac/petchopp/PetchoppApplication.java
+++ petchopp/src/main/java/com/senac/petchopp/PetchoppApplication.java
@@ -2,9 +2,10 @@ package com.senac.petchopp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PetchoppApplication {
+public class PetchoppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetchoppApplication.class, args);
