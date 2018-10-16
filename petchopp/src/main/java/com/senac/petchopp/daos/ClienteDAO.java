e2043a82225bf8b7ee8736dc851a66397faf7dbb
Marcelo Pereira <macope727@gmail.com>
2018-10-16 19:43:33 -0300

    Não lembro mais

--- petchopp/src/main/java/com/senac/petchopp/daos/ClienteDAO.java
+++ petchopp/src/main/java/com/senac/petchopp/daos/ClienteDAO.java
@@ -19,68 +19,43 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author marcelo
+ * @author Marcelo Pereira
  */
 public class ClienteDAO implements IDAO{
     Connection cn = null;
+
+	@Override
+	public void salvar(Object bean) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Object bean) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletar(long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object getById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Object> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
    
 
-    @Override
-    public void salvar(Object bean) {
-        PreparedStatement stmt = null;
-        
-        String sql = " INSERT INTO CLIENTE (idCliente, dtCadastro, Nome, dtNasc, RG, CPF"
-                + "Email, Senha, Ativo) VALUES (?,?,?,?,?,?,?,?,?)";
-        
-        try {
-            Cliente cliente = (Cliente) bean;
-            stmt = cn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            stmt.setLong(1, cliente.getIdCliente());
-            stmt.setDate(2, new java.sql.Date(Auxiliares.UtilDateToCalendar(cliente.getDtCadastro()).getTimeInMillis()));
-            stmt.setString(3, cliente.getNome());
-            stmt.setDate(4, new java.sql.Date(Auxiliares.UtilDateToCalendar(cliente.getDtNasc()).getTimeInMillis()));
-            stmt.setString(0, sql);
-            
-//            Pega o id do cliente
-            ResultSet rs = stmt.getGeneratedKeys();
-            int i = 0;
-            if (rs.next()) {
-                i = rs.getInt(1);
-                System.out.println(i);
-                cliente.setIdCliente(i);
-                System.out.println(cliente.getIdCliente());
-            }
-
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } 
-        finally {
-        }
-        
-    }
-
-    @Override
-    public void atualizar(Object bean) {
-         
-    }
-
-    @Override
-    public void deletar(int id) {
-         
-    }
-
-    @Override
-    public Object getById(int id) {
-        
-        return null;
-    }
-
-    @Override
-    public List<Object> getAll() {
-        
-        return null;
-    }
+   
     
     
 }
