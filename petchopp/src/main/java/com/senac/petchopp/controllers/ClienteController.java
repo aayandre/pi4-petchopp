8b97c0916d63c7ade5f25561ba23fb1b775b948e
Marcelo Pereira <macope727@gmail.com>
2018-10-30 13:02:41 -0300

    Trilha Cliente - teste cadastro

--- petchopp/src/main/java/com/senac/petchopp/controllers/ClienteController.java
+++ petchopp/src/main/java/com/senac/petchopp/controllers/ClienteController.java
@@ -3,14 +3,23 @@ package com.senac.petchopp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.senac.petchopp.model.cliente.Cliente;
 
 @Controller
 @RequestMapping("cliente")
 public class ClienteController {
 	
-	@GetMapping("/Cadastro")
+	@GetMapping("Cadastro")
 	public String novoCliente() {
-		return "novoCliente";
+		Cliente cli = new Cliente();
+		return "cliente-index";
+	}
+	
+	@GetMapping("NCadastro")
+	public ModelAndView formCliente() {
+		return new ModelAndView("novoCliente").addObject("cliente", new Cliente());
 	}
 	
 	 
