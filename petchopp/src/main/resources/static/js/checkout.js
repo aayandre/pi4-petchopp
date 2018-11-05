4795532188b9fc45d85e9a63845d8a51dbbdbe3d
Leandrowisky <leandro10le@hotmail.com>
2018-11-04 23:11:59 -0200

    página de checkout, finalizado o layout, falta js para controlar a forma de pagamento e alterar os códigos para o padrão thymeleaf

---
+++ petchopp/src/main/resources/static/js/checkout.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#nr-cartao-cred").mask('0000 0000 0000 0000')
+    $("#venc-cartao-cred").mask('00/00')
+    $("#nr-cartao-deb").mask('0000 0000 0000 0000')
+    $("#venc-cartao-deb").mask('00/00')
+  });
