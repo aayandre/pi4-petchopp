b46de04c6332c507f565f1bcda00f9a5f0f9f4ad
LeandrOrgm <leandro.alves@orgm.com.br>
2018-11-05 21:47:31 -0200

    detalhes do checkout, feito js para controle das formas de pagamento.

--- petchopp/src/main/resources/static/js/checkout.js
+++ petchopp/src/main/resources/static/js/checkout.js
@@ -1,6 +1,52 @@
-$(document).ready(function(){
-    $("#nr-cartao-cred").mask('0000 0000 0000 0000')
-    $("#venc-cartao-cred").mask('00/00')
-    $("#nr-cartao-deb").mask('0000 0000 0000 0000')
-    $("#venc-cartao-deb").mask('00/00')
+$(document).ready(function () {
+  $("#nr-cartao-cred").mask('0000 0000 0000 0000');
+  $("#venc-cartao-cred").mask('00/00');
+  $("#nr-cartao-deb").mask('0000 0000 0000 0000');
+  $("#venc-cartao-deb").mask('00/00');
+
+  $('#typePayment-cred').click(function () {
+    if ($('#typePayment-cred').is(':checked')) {
+
+      $("#pag-info-cred").removeClass("box-pag-display");
+
+      if (!$("#pag-info-deb").hasClass('box-pag-display')) {
+        $("#pag-info-deb").addClass("box-pag-display");
+      }
+
+      if (!$("#pag-info-boleto").hasClass('box-pag-display')) {
+        $("#pag-info-boleto").addClass("box-pag-display");
+      }
+    }
   });
+
+  $('#typePayment-deb').click(function () {
+    if ($('#typePayment-deb').is(':checked')) {
+
+      $("#pag-info-deb").removeClass("box-pag-display");
+
+      if (!$("#pag-info-cred").hasClass('box-pag-display')) {
+        $("#pag-info-cred").addClass("box-pag-display");
+      }
+
+      if (!$("#pag-info-boleto").hasClass('box-pag-display')) {
+        $("#pag-info-boleto").addClass("box-pag-display");
+      }
+    }
+  });
+
+  $('#typePayment-boleto').click(function () {
+    if ($('#typePayment-boleto').is(':checked')) {
+
+      $("#pag-info-boleto").removeClass("box-pag-display");
+
+      if (!$("#pag-info-cred").hasClass('box-pag-display')) {
+        $("#pag-info-cred").addClass("box-pag-display");
+      }
+
+      if (!$("#pag-info-deb").hasClass('box-pag-display')) {
+        $("#pag-info-deb").addClass("box-pag-display");
+      }
+    }
+  });
+});
+
