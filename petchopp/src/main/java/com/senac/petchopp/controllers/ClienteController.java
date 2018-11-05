05a469eded662123eafba25fe13da585bdee0d83
Marcelo Pereira <macope727@gmail.com>
2018-11-05 20:08:52 -0200

    Cadastro cliente + Login

--- petchopp/src/main/java/com/senac/petchopp/controllers/ClienteController.java
+++ petchopp/src/main/java/com/senac/petchopp/controllers/ClienteController.java
@@ -3,16 +3,25 @@ package com.senac.petchopp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.senac.petchopp.model.cliente.Cliente;
 
 @Controller
 @RequestMapping("cliente")
 public class ClienteController {
 	
-	@GetMapping("/Cadastro")
-	public String novoCliente() {
-		return "novoCliente";
+	@GetMapping("conta")
+	public String minhaConta() {
+		return "cliente-index";
 	}
 	
-	 
-	
+	@GetMapping("Cadastro")
+	public ModelAndView novoCliente() {
+		ModelAndView modelAndView = new ModelAndView("novoCliente");
+		modelAndView.addObject("cliente", new Cliente());
+		System.out.println("Entrando em CadastroCliente");
+		return modelAndView;
+	}
+		
 }
