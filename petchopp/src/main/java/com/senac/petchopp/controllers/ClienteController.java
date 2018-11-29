0ced937e274c60407dd9fd66a5509743cce333f1
LeandrOrgm <leandro.alves@orgm.com.br>
2018-11-29 21:53:29 -0200

    Merge origin/todayy into Leandro
    Conflicts:
    	petchopp/src/main/java/com/senac/petchopp/controllers/ClienteController.java
    	petchopp/src/main/resources/static/css/orderList.css
    	petchopp/src/main/resources/static/js/orderList.js
    	petchopp/src/main/resources/templates/cli/orderList.html

--- petchopp/src/main/java/com/senac/petchopp/controllers/ClienteController.java
+++ petchopp/src/main/java/com/senac/petchopp/controllers/ClienteController.java
@@ -17,10 +17,13 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.senac.petchopp.daos.ClienteDAO;
+import com.senac.petchopp.daos.VendaDAO;
 import com.senac.petchopp.model.Auxiliares;
 import com.senac.petchopp.model.cliente.Cliente;
 import com.senac.petchopp.model.cliente.Endereco;
+import com.senac.petchopp.model.venda.Venda;
 import com.senac.petchopp.service.LoginService;
+import java.util.List;
 
 @Controller
 @RequestMapping("cliente")
